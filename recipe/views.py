4338d151f05976e890bb39fd1679900e3b274ff8
Nathan Tinius <nathantinius@gmail.com>
2019-04-04 20:03:41 -0500

    Homepage, bootstrap, view and html

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Recipe
+
+def home(request):
+    recipes = Recipe.objects
+    return render(request, 'recipe/home.html', {'recipes': recipes})
--- slate_site/urls.py
+++ slate_site/urls.py
@@ -18,7 +18,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+import recipe.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', recipe.views.home),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
